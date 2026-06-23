44e235ef069ed2cc8760aab5d835c5c33cd12660
KHUETECH <buinhukhue202@gmail.com>
2026-06-23 16:04:34 +0700

    cập nhật flyway config và init data

---
+++ core/src/main/resources/db/migration/V3__init_role.sql
@@ -0,0 +1,3 @@
+-- Role table
+INSERT INTO public."role" (id,created_at,created_by,updated_at,updated_by,default_assign,description,name) VALUES
+	 ('80a432e5-455a-4232-9a90-9da219a0a543'::uuid,'2026-06-23 14:42:49.424','1e6633fb-2654-4bd5-aa7d-51bb86418987'::uuid,'2026-06-23 14:42:49.424','1e6633fb-2654-4bd5-aa7d-51bb86418987'::uuid,false,'Quyền cho tài khoản Root','ROOT');
